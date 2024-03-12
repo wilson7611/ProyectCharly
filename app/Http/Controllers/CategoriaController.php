8fc94da2893e6ea1da57880472863fafad0b32ad
wilson7611 <106674260+wilson7611@users.noreply.github.com>
2024-03-12 01:03:20 -0400

    crud Categoria

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+
+class CategoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categorias = Categoria::all();
+        return response()->json($categorias);
+    }
+
+  
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $categoria = Categoria::create($request->all());
+        return response()->json($categoria, 201);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request,  $id)
+    {
+        $categoria = Categoria::findOrFail($id);
+        $categoria->update($request->all());
+        return response()->json($categoria, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $categoria = Categoria::findOrFail($id);
+        $categoria->delete();
+        return response()->json(null, 204);
+    }
+}
